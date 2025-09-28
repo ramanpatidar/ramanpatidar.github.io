82eda3e44255d2c3ea83286748de47f0169c96fa
ramanpatidar <ramanpatidar36@gmail.com>
2025-09-28 15:14:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("✅ Your booking request has been submitted successfully! I will contact you soon.");
-});
