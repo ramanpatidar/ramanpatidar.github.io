1c8dcc723bff87ee424715e3abce2549d72f679f
ramanpatidar <ramanpatidar36@gmail.com>
2025-09-28 13:22:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("bookingForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("✅ Your booking request has been submitted successfully! I will contact you soon.");
+});
